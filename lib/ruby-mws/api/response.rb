9582479af796e89c6db6c559ca3c2044688503be
Erik Lyngved <elyngved@gmail.com>
2012-01-16 19:00:58 -0500

    Tidying Response class

--- lib/ruby-mws/api/response.rb
+++ lib/ruby-mws/api/response.rb
@@ -6,11 +6,12 @@ module MWS
       def self.parse(hash, name, params)
         rash = self.new(hash)
         raise BadResponseError, "received non-matching response type #{rash.keys}" if rash["#{name}_response"].nil?
-        rash = rash[rash.keys.first]
+        rash = rash["#{name}_response"]
 
         if rash["#{name}_result"]
           rash = rash["#{name}_result"]
-          params[:mods].each {|mod| mod.call(rash) } if params[:mods]
+          # only runs mods if correct result is present
+          params[:mods].each {|mod| mod.call(rash) } if params[:mods
           rash
         end
       end
