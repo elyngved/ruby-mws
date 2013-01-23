3df1c481e5c9d28c249d0eef733c4f6979923026
Alex Dowad <alexinbeijing@gmail.com>
2013-01-23 13:22:08 +0200

    Move logic for dealing with XML feeds into new Feeds module. Add MWS::API::Inventory#update_inventory_prices

--- lib/ruby-mws/api/inventory.rb
+++ lib/ruby-mws/api/inventory.rb
@@ -4,6 +4,7 @@ module MWS
   module API
 
     class Inventory < Base
+      include Feeds
 
       def_request [:list_inventory_supply, :list_inventory_supply_by_next_token],
         :verb => :get,
@@ -45,38 +46,37 @@ module MWS
           end
         end
 
-        response = send_request(:submit_feed, :feed_type => '_POST_INVENTORY_AVAILABILITY_DATA_', :body => xml, :verb => :post, :content_md5 => true)
+        submit_feed('_POST_INVENTORY_AVAILABILITY_DATA_', xml)
+      end
 
-        # Amazon returns FeedSubmissionId, which we use to check if submission was successful
-        submission_id = response.feed_submission_info.feed_submission_id
-
-        # first wait until Amazon is finished processing the feed
-        # this usually takes at least something close to a minute
-        # so don't check back too frequently -- otherwise our FeedSubmissionList
-        #   queries will be throttled
-        while true
-          sleep(30)
-          response = send_request(:get_feed_submission_list, :lists => {:feed_submission_ids => "FeedSubmissionIdList.Id"}, :feed_submission_ids => [submission_id])
-          break if response.feed_submission_info.feed_processing_status == '_DONE_'
-        end
-
-        # then check whether there were any errors
-        response = send_request(:get_feed_submission_result, :feed_submission_id => submission_id)
-
-        errors = ""
-        [*response.message.processing_report.result].each do |result|
-          if %w{Warning Error}.include? result.result_code
-            errors << result.result_description
-            if result.additional_info && !result.additional_info.empty?
-              errors << " (additional info: "
-              errors << result.additional_info.map { |k,v| "#{k}=#{v}" }.join(' ')
-              errors << ")"
+      # Takes a currency code and a hash of SKUs -> prices
+      # Returns true if all the products were updated successfully
+      # Otherwise raises an exception
+      def update_inventory_prices(merchant_id, currency_code, hash)
+        # schema for XML "feed" is available at:
+        # https://images-na.ssl-images-amazon.com/images/G/01/rainier/help/XML_Documentation_Intl.pdf
+        xml = ""
+        builder = Builder::XmlMarkup.new(:indent => 2, :target => xml)
+        builder.instruct!
+        builder.AmazonEnvelope(:"xmlns:xsi" => "http://www.w3.org/2001/XMLSchema-instance", :"xsi:noNamespaceSchemaLocation" => "amzn-envelope.xsd") do |env|
+          env.Header do |head|
+            head.DocumentVersion('1.01')
+            head.MerchantIdentifier(merchant_id)
+          end
+          env.MessageType('Price')
+          i = 0
+          hash.each do |sku,price|
+            env.Message do |msg|
+              msg.MessageID(i += 1)
+              msg.Price do |pr|
+                pr.SKU(sku)
+                pr.StandardPrice(price, 'currency' => currency_code)
+              end
             end
-            errors << "\n"
           end
         end
 
-        errors.empty? || (raise ErrorResponse, errors)
+        submit_feed('_POST_PRODUCT_PRICING_DATA_', xml)
       end
     end
 
