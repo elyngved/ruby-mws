73af8282232e28ffde06715fc81ab30fbc58f85f
Pierre Larochelle <pierrebombay@gmail.com>
2014-02-11 16:02:22 -0800

    trying to add md5 header, not quite right yet

--- lib/ruby-mws/api/feed.rb
+++ lib/ruby-mws/api/feed.rb
@@ -1,15 +1,19 @@
+require 'digest/md5'
+
 module MWS
   module API
     class Feed < Base
-      def submit_feed
+      def submit_feed(query_params={})
+        query_params = {:feed_type => '_POST_ORDER_ACKNOWLEDGEMENT_DATA_'}
         options = {
           :verb => :post,
           :uri => '/',
           :version => '2009-01-01'
         }
-        params = [default_params('submit_feed'), options, @connection.to_hash].inject :merge
+        params = [default_params('submit_feed'), query_params, options, @connection.to_hash].inject :merge
         query = Query.new params
-        resp = self.class.post(query.request_uri)
+        body = ''
+        resp = self.class.post(query.request_uri, :body => body, :headers => {'Content-MD5' => Digest::MD5.hexdigest(body)})
       end
     end
   end
