fd4633630e728e480b65e2c813dd39603d762be9
Will Holcomb <will@dhappy.org>
2014-02-12 16:39:13 -0500

    trying to move reformatting to gem

--- lib/ruby-mws/api/product.rb
+++ lib/ruby-mws/api/product.rb
@@ -5,7 +5,17 @@ module MWS
       def_request [:get_matching_product],
         verb: :get,
         uri: '/Products/2011-10-01',
-        version: '2011-10-01'
+        version: '2011-10-01',
+        mods: [
+          lambda do |response|
+            response.products = response.product.map do |p|
+              p = p[0]
+              response = p[:product][:attribute_sets][:item_attributes]
+              response[:asin] = p[:asin]
+              response
+            end
+          end
+        ]
     end
   end
 end
