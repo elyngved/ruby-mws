086c9bfcc7a8e7b4968bd9ab10489d79e2ebf894
Will Holcomb <will@dhappy.org>
2014-02-12 16:16:48 -0500

    syntax error

--- lib/ruby-mws/api/product.rb
+++ lib/ruby-mws/api/product.rb
@@ -2,8 +2,7 @@ module MWS
   module API
 
     class Product < Base
-      def_request
-        [:get_matching_product],
+      def_request [:get_matching_product],
         verb: :get,
         uri: '/Products/2011-10-01',
         version: '2011-10-01'
