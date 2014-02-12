662dba200af6bb385830f6186247f79e75dee500
Pierre Larochelle <pierrebombay@gmail.com>
2014-02-11 17:23:34 -0800

    adding example body and trying file-based md5 calc

--- lib/ruby-mws/api/feed.rb
+++ lib/ruby-mws/api/feed.rb
@@ -1,4 +1,5 @@
 require 'digest/md5'
+require 'tempfile'
 
 module MWS
   module API
@@ -12,8 +13,42 @@ module MWS
         }
         params = [default_params('submit_feed'), query_params, options, @connection.to_hash].inject :merge
         query = Query.new params
-        body = ''
-        resp = self.class.post(query.request_uri, :body => body, :headers => {'Content-MD5' => Digest::MD5.hexdigest(body)})
+        body = example_body
+
+        # write body to file to get md5 hash
+        file = Tempfile.new('mws')
+        file.write(body)
+        file.close
+
+        resp = self.class.post(query.request_uri, :body => body, :headers => {'Content-MD5' => Digest::MD5.file(file.path).hexdigest})
+        file.unlink
+
+        resp
+      end
+
+      def example_body
+        body = <<-BODY
+<?xml version="1.0"?> 
+<AmazonEnvelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="amzn-envelope.xsd"> 
+<Header> 
+<DocumentVersion>1.01</DocumentVersion> 
+<MerchantIdentifier>A3IGWI0VX4DJJM</MerchantIdentifier> 
+</Header> 
+<MessageType>OrderAcknowledgment</MessageType> 
+<Message> 
+<MessageID>1</MessageID> 
+<OrderAcknowledgement> 
+<AmazonOrderID>050-1234567-1234567</AmazonOrderID> 
+<MerchantOrderID>1234567</MerchantOrderID> 
+<StatusCode>Success</StatusCode> 
+<Item> 
+<AmazonOrderItemCode>12345678901234</AmazonOrderItemCode> 
+<MerchantOrderItemID>1234567</MerchantOrderItemID> 
+</Item> 
+</OrderAcknowledgment> 
+</Message> 
+</AmazonEnvelope>
+BODY
       end
     end
   end
