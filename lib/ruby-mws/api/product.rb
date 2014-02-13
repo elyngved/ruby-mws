8fda1bbe20860ee893029b5372ba4d9fcdd93f08
Will Holcomb <will@dhappy.org>
2014-02-12 20:13:47 -0500

    adding additional endpoints

--- lib/ruby-mws/api/product.rb
+++ lib/ruby-mws/api/product.rb
@@ -7,10 +7,48 @@ module MWS
         version: '2011-10-01',
         mods: [
           lambda do |response|
-            puts response
-            response.product.map { |p| p[0][:attribute_sets][:item_attributes] }
+            response.map! do |p|
+              n = p.product.attribute_sets.item_attributes
+              n.asin = p.asin
+              n
+            end
           end
         ]
+
+      def_request [:get_competitive_pricing_for_ASIN],
+        verb: :get,
+        uri: '/Products/2011-10-01',
+        version: '2011-10-01',
+        mods: [
+          lambda do |response|
+            response.map! do |p|
+              n = p.product.competitive_pricing
+              n.prices = n.competitive_prices.competitive_price
+              n.sales_rankings = p.product.sales_rankings
+              n.asin = p.asin
+              n
+            end
+          end
+        ]
+
+      def_request [:get_lowest_offer_listings_for_ASIN],
+        verb: :get,
+        uri: '/Products/2011-10-01',
+        version: '2011-10-01',
+        mods: [
+          lambda do |response|
+            response.map! do |p|
+              n = p.product.lowest_offer_listings
+              n.asin = p.asin
+              n
+            end
+          end
+        ]
+
+      def_request [:get_product_categories_for_ASIN],
+        verb: :get,
+        uri: '/Products/2011-10-01',
+        version: '2011-10-01'
     end
   end
 end
