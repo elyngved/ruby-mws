152c089d8f2c0f545e1b005dad03174c9fe2eea2
Pierre Larochelle <pierrebombay@gmail.com>
2014-02-19 08:50:50 -0800

    updating feed and order spec to use let instead of instance vars

--- spec/ruby-mws/api/feed_spec.rb
+++ spec/ruby-mws/api/feed_spec.rb
@@ -4,13 +4,14 @@ describe MWS::API::Feed do
 
   before :all do
     EphemeralResponse.activate
-    @mws = MWS.new(auth_params)
   end
 
   context "requests" do
+    let(:mws) {@mws = MWS.new(auth_params)}
+
     describe "submit_feed" do
       it "should be able to ack an order" do
-        response = @mws.feeds.submit_feed(MWS::API::Feed::ORDER_ACK, {
+        response = mws.feeds.submit_feed(MWS::API::Feed::ORDER_ACK, {
                                             :amazon_order_id => '105-1063273-7151427'
                                           })
         response.feed_submission_info.should_not be_nil
@@ -21,7 +22,7 @@ describe MWS::API::Feed do
       end
 
       it "should be able to ack a shipment" do
-        response = @mws.feeds.submit_feed(MWS::API::Feed::SHIP_ACK, {
+        response = mws.feeds.submit_feed(MWS::API::Feed::SHIP_ACK, {
                                             :amazon_order_id => '105-8268075-6520231',
                                             :shipping_method => '2nd Day',
                                             :items => [
