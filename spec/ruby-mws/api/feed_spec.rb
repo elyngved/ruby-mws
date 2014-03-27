8dc4837d4549b9b3a64b9429c913391dbdffeaaf
Pierre Larochelle <plarochelle@blurb.com>
2014-03-27 09:37:30 -0700

    Adding tests for order ack xml structure

--- spec/ruby-mws/api/feed_spec.rb
+++ spec/ruby-mws/api/feed_spec.rb
@@ -10,6 +10,7 @@ describe MWS::API::Feed do
     let(:mws) {@mws = MWS.new(auth_params)}
     let(:first_order_id) {'105-8268075-6520231'}
     let(:second_order_id) {'105-8268075-6520232'}
+    let(:order_ack_order_id){'105-1063273-7151427'}
     let(:shipment_hash) {
       {
         :orders => [ {
@@ -43,18 +44,43 @@ describe MWS::API::Feed do
                      } ]
       }
     }
+    let(:merchant_order_id){"10"}
+    let(:merchant_order_item_code){"123"}
+    let(:amazon_order_item_code){"10-31-123123"}
+    let(:order_hash){{
+      :amazon_order_id => order_ack_order_id,
+      :merchant_order_id => merchant_order_id,
+      :amazon_order_item_code => amazon_order_item_code,
+      :merchant_order_item_id => merchant_order_item_code
+    }}
 
     describe "submit_feed" do
       it "should be able to ack an order" do
-        response = mws.feeds.submit_feed(MWS::API::Feed::ORDER_ACK, {
-                                            :amazon_order_id => '105-1063273-7151427',
-                                            :merchant_order_id => 10
-                                          })
+        response = mws.feeds.submit_feed(MWS::API::Feed::ORDER_ACK, order_hash)
         response.feed_submission_info.should_not be_nil
 
         info = response.feed_submission_info
         info.feed_processing_status.should == "_SUBMITTED_"
         info.feed_type.should == MWS::API::Feed::ORDER_ACK
+      end
+
+      it "should create the correctx body for an order" do
+        MWS::API::Feed.should_receive(:post) do |uri, hash|
+          hash.should include(:body)
+          body = hash[:body]
+          
+          body_doc = Nokogiri.parse(body)
+          body_doc.css('AmazonEnvelope MessageType').text.should == "OrderAcknowledgement"
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement').should_not be_empty
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement AmazonOrderID').text.should == order_ack_order_id
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement MerchantOrderID').text.should == merchant_order_id
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement StatusCode').text.should == "Success"
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement Item').should_not be_empty
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement Item AmazonOrderItemCode').text.should == amazon_order_item_code
+          body_doc.css('AmazonEnvelope Message OrderAcknowledgement Item MerchantOrderItemID').text.should == merchant_order_item_code
+        end
+        
+        response = mws.feeds.submit_feed(MWS::API::Feed::ORDER_ACK, order_hash)
       end
 
       it "should be able to ack a shipment" do
